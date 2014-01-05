cd95ab5fab226af984b3d5e41675454227fc870f
MeMpy <rosariorenga@gmail.com>
2014-01-05 19:49:34 +0100

    New design for ReadersMappingDocuments

---
+++ src/rmdocs/models/OraclePackageModels.py
@@ -0,0 +1,245 @@
+'''
+Created on Jan 5, 2014
+
+@author: ross
+'''
+from rmdocs.RmDoc import IRmDoc
+import os
+import re
+
+PTRN_WHITE_SPACE_SUBS = r' +'
+PTRN_PROCEDURE_NAME = r'\b\w*\s*\('
+PTRN_PARAMETERS_BLOCK = r'\(.*\)' #Works only if there aren't parameters with comments (into which there is a ")")
+
+SPLIT_PARAMETER_TOKEN = ','
+
+FIND_START_PARAMETER_DECLARATION = "P"
+
+class OraclePackageDoc(IRmDoc):
+    '''
+    classdocs
+    '''
+    
+    class Procedure(object):
+        
+        class Parameter(object):
+            
+            def __init__(self, param_name, sqltype, inoutType ):
+                self.param_name = param_name
+                self.sql_type = sqltype
+                self.inout_type = inoutType
+            
+            def __unicode__(self):
+                return str(
+                           str(self.param_name) + ' ' +
+                           str(self.inout_type) + ':' + 
+                           str(self.sql_type))
+              
+            def __str__(self):
+                return self.__unicode__()
+                
+                                    
+        def __init__(self, procName = None, index = None, headerComment = ""):
+            '''
+            Rapresents a Procedure reader form a oracle body. It contains:
+            - The Procedure Name
+            - An identifier (or kind) made from the procName
+            - The header comments if any
+            - The procedure parameters if any, for each parameters will be stored:
+                - the name
+                - the type of input/output
+                - the type of the parameter
+            '''
+            self.procName = procName
+            self.identifier = self._buildIdentifier()
+            self.headerComment = headerComment
+            #the zero-based line number in which the procedure is written
+            self.procStartIndex = index
+            #(param name :  param type, in out type)
+            self.procParams = []
+            
+        def _buildIdentifier(self):
+            identifier = None
+            if self.procName:
+                startIndex = 0
+                if self.procName.startswith("GET_"):
+                    startIndex = len("GET_")-1    
+                names = self.procName[startIndex+1:].split("_")
+                identifier = str()
+                for name in names:
+                    identifier +=name[:3].title()            
+            return identifier       
+        
+        def _addProcParam(self, name, sqltype, inoutType):
+            self.procParams.append(OraclePackageDoc.Procedure.Parameter(name, sqltype, inoutType))
+            
+        def addProcParamFromList(self, paramsList):
+            '''
+            @param paramsList: must be as follows: [paramName, in out type, type] 
+            '''
+            for param in paramsList:
+                if len(param) == 3:
+                    #there is the input output modifier
+                    self._addProcParam(param[0], param[2], param[1])
+                elif len(param) == 2:
+                    #there isn't the input output modifier
+                    self._addProcParam(param[0], param[1], str())
+                else:
+                    raise Exception("{0} Parameters not well-formed".format(self.procName))
+        
+        #Utility   
+        def get_parameters_pames(self):
+            return self.procParams.keys()
+        
+        def get_input_parameters_count(self):
+            inputParams = [x for x in self.procParams.values() if "IN" in x[1].upper()]
+            return len(inputParams)
+        
+        def __unicode__(self):
+            return str(
+                       str(self.identifier) + os.linesep +
+                       str(self.procStartIndex) + os.linesep + 
+                       str(self.procName) + os.linesep 
+                       + str(map(str,self.procParams)))
+        
+        def __str__(self):
+            return self.__unicode__()
+                                
+    
+    def __init__(self, procs_data):
+        
+        #Build a list of Procedure objects
+        proceduresList = self._build_procedures_list(procs_data)
+        #Build the internal structure which manage the procedure objects
+        self._proceduresMap = self._buildProceduresMap(proceduresList)
+        
+        #Initialize the procedure names subset to empty list      
+        self._proceduresSubSet = list()
+        
+        self.packageName = "packageName" #TODO package name
+        
+        #Build the attribute that will be used by the user
+        self.procedures = self._build_procedures()
+    
+    
+    def _buildProceduresMap(self, proceduresList):
+        procsMap = dict()
+        for proc in proceduresList:
+            procsMap[proc.identifier] = proc
+        return procsMap
+    
+    def _build_procedures(self):
+        """
+        """
+        proceduresSubSet = self._proceduresMap.values()
+        if self._proceduresSubSet:
+            proceduresSubSet = [x for x in self._proceduresMap.values() if x.procName in self._proceduresSubSet]
+        return sorted(proceduresSubSet, key = lambda proc: proc.procStartIndex)    
+        
+    def _build_procedures_list(self, procList):
+        """
+        @param procList: It is a list of tuple,
+        each tuple is defined as follows:
+        (procIndex, commentBlock, SignatureBlock, BodyBlock)
+        For each tuple will be called:
+        _add_comment_to_proc_obj
+        _add_signature_to_proc_obj
+        _add_body_to_proc_obj
+        In order to build a procedure object        
+        """
+        proceduresList = list()
+        procObj = None
+         
+        for p in procList:
+            procObj = OraclePackageDoc.Procedure()
+            self._add_comment_to_proc_obj(p[1], procObj)
+            self._add_signature_to_proc_obj(p[0], p[2], procObj)
+            self._add_body_to_proc_obj(p[3], procObj)
+            procObj.identifier= procObj._buildIdentifier()        
+            proceduresList.append(procObj)
+        
+        return proceduresList    
+    
+    
+    def _add_comment_to_proc_obj(self, commentBlock, proc):
+        """
+        """
+        pass    
+    
+    def _add_signature_to_proc_obj(self, procIndex, segnatureBlock, proc):
+        """        
+        From the signatureBlock read:
+        - the procedure name
+        - the parameters block
+        
+        Add them to the procedure object
+                    
+        """
+                      
+        match = re.search(PTRN_PROCEDURE_NAME, segnatureBlock)
+        procName = match.group()[:-1]            
+        match = re.search(PTRN_PARAMETERS_BLOCK, segnatureBlock, flags = re.S) #flags = re.DOTALL
+        paramsBlock = match.group()[1:-1]
+        
+        #=======================================================================
+        # Parameters Parsing
+        #=======================================================================
+        rowParamsList = paramsBlock.split(SPLIT_PARAMETER_TOKEN)
+        params = list()        
+        rowParam = str()
+        startIndex = None
+        for rowParam in rowParamsList:        
+            startIndex = rowParam.upper().find(FIND_START_PARAMETER_DECLARATION)                
+            #first three elements of the list obtained splitting rowParam by a n-whitespace token
+            params += [rowParam[startIndex:].split()]
+        
+        proc.procName = procName
+        proc.procStartIndex = procIndex
+        proc.addProcParamFromList(params)      
+        
+    
+    
+    def _add_body_to_proc_obj(self, bodyBlock, proc):
+        """
+        """
+        pass
+    
+    #Utility
+    def GetProceduresNames(self):
+        return [x.procName for x in self.GetProcedures()]
+    
+    def GetProceduresKinds(self):
+        return [(x.identifier, x.procName) for x in self.GetProcedures()]
+    
+    def GetPackageName(self):
+        return self._packageName
+    
+    def SetNewIdentifier(self, oldIdentifier, newIdentifier):
+        procTemp = self._proceduresMap.pop(oldIdentifier)
+        if self._proceduresMap.has_key(newIdentifier):
+            raise Exception("Identifier {0} already used".format(newIdentifier))
+        else:
+            procTemp.identifier = newIdentifier
+            self._proceduresMap[newIdentifier] = procTemp
+    
+    def set_procedures_subset(self, proceduresNames):
+        """
+        Set the procedures sub set.
+        It is used to filter the procedures returned by gets methods.  
+        It must be a list of procedure names contained into the package
+        Each time a set is added the procedures list into this reader instance will be rebuild      
+        """ 
+        if type(proceduresNames) is list:
+            self._proceduresSubSet = proceduresNames
+            self.procedures = self._build_procedures()
+        else:
+            raise Exception("proceduresNames isn't a list")
+        
+    def __unicode__(self):
+        result = [self.packageName]
+        for p in self.procedures:
+            result.append(str(p))
+        return os.linesep.join(result)
+    
+    def __str__(self):
+        return self.__unicode__()
---
+++ src/rmdocs/parsers/OraclePackageParser.py
@@ -0,0 +1,209 @@
+# -*- coding: UTF-8 -*-
+'''
+Created on Jan 5, 2014
+
+@author: ross
+'''
+import re
+
+PTRN_SINGLE_LINE_COMMENT = r'\s*--'
+PTRN_BEGIN_BLOCK_COMMENT = r'\s*/\*'
+PTRN_END_BLOCK_COMMENT = r'\*/'
+PTRN_SINGLE_LINE_COMMENT_SUBS = r'\s*--.*'
+PTRN_BLOCK_COMMENT_SUBS = r'\s*/\*.*\*/'
+PTRN_START_PROCEDURE_BLOCK = r'\s*PROCEDURE'
+PTRN_START_FUNCTION_BLOCK = r'\s*FUNCTION'
+PTRN_END_PROC_FUNC_BLOCK_BRACKET = r'\)\s*\n'
+PTRN_END_PROC_FUNC_BLOCK_IS = r'\bIS\b'
+PTRN_END_PROC_FUNC_BLOCK_AS = r'\bAS\b'
+
+SINGLE_WHITE_SPACE = ' '
+
+def _clean_line_from_comments(line):
+    """
+    Clean the line from single line comment using regular expression
+    """
+    lineNoComment = re.sub(PTRN_SINGLE_LINE_COMMENT_SUBS, SINGLE_WHITE_SPACE, line)
+    lineNoComment = re.sub(PTRN_BLOCK_COMMENT_SUBS, SINGLE_WHITE_SPACE, lineNoComment)
+    return lineNoComment
+
+def _check_comment(line, isInCommentBlock):
+        """
+        Check if we are still in a comment block or
+        if we are in a comment line
+        or if we are entering or leaving a comment bock
+        using regular expression (default behavior)
+        """
+        isComment = False
+        isBlockComment = isInCommentBlock
+        if not isBlockComment: #we don't come from a comment block
+            #check if it a line comment or a block comment on a single line
+            if (re.match(PTRN_SINGLE_LINE_COMMENT, line) or
+                (re.match(PTRN_BEGIN_BLOCK_COMMENT, line) and
+                 re.search(PTRN_END_BLOCK_COMMENT, line))):
+                isComment = True
+            else:
+                isComment = False        
+                if re.match(PTRN_BEGIN_BLOCK_COMMENT, line):
+                    isBlockComment = True #we are entering in a comment block
+        elif re.search(PTRN_END_BLOCK_COMMENT, line): #we come from a block comment
+            #we are leaving from a block comment
+            isBlockComment = False
+        return isComment, isBlockComment
+    
+def _check_is_in_signature(line, isInSignature):
+        """
+        Check if we are still in a signature block 
+        or if we are entering or leaving a signature bock
+        using regular expression (default behavior)
+        """            
+        #Clean the line from the single line comment        
+        lineNoComment = _clean_line_from_comments(line)
+        if not isInSignature: #We don't come from a signature block         
+            #Check if the line contains the beginning of a procedure
+            if re.match(PTRN_START_PROCEDURE_BLOCK, lineNoComment, re.I) or re.match(PTRN_START_FUNCTION_BLOCK, lineNoComment, re.I):
+                return True
+            else:
+                return False
+        #We come from a signature block
+        elif (re.search(PTRN_END_PROC_FUNC_BLOCK_BRACKET, lineNoComment) or
+              re.search(PTRN_END_PROC_FUNC_BLOCK_IS, lineNoComment) or
+              re.search(PTRN_END_PROC_FUNC_BLOCK_AS, lineNoComment)):
+            #if the signature block ends
+            return False
+        else:
+            #else we still are in a signature block
+            return True
+
+def _check_is_in_body_block(line, isInBody):
+    """
+    This isn't implemented in the default behavior
+    """
+    return False
+
+def _build_comment_block(line, index):
+    """
+    Not Implemented in default behavior
+    """
+    return ''
+
+
+def _build_signature_block(line, signatureLineIndex, procSignature, index = None):
+    """
+    Simply append the new signature line to the buffer 
+    NOTE: this also append the last line when isInSignature become false 
+    (default behavior)
+    @param index: The index of the actual line
+    """
+    #This check is needed because we can call this method more than once for a single line
+    if signatureLineIndex != index:
+        procSignature += _clean_line_from_comments(line)
+        signatureLineIndex = index
+    
+    return procSignature, signatureLineIndex
+
+def _build_procedure_body_block(line, index = None):
+    """
+    This isn't implemented in the default behavior
+    """
+    return ''
+
+
+
+def parse(f, process_body = False):
+    """    
+    Legge dal body di un package sql oracle tutte le 
+    dichiarazioni di procedure e funzioni e le memorizza 
+    in una lista come stringhe    
+    Inoltre coserva il numero di linea in cui è
+    stata trovata la procedura/funzione
+    
+    Algoritmo:
+    Per ogni linea che non sia un comment block o una linea di commento:
+        1)Pulisci la linea da eventuali commenti
+        2)Se nella linea si trova la dichiarazione di una procedure/funzione allora
+            -Inizia a memorizzare le linee che trovi
+            fino a quando la linea corrente non contiene un terminatore della dichiarazione
+            @see PTRN_END_PROC_FUNC_*
+        3)altrimenti ignora la linea
+    ritorna la lista con i blocchi creati
+    
+    NOTA: Al momento non sono presi in considerazione il blocco dei commenti in testa alla procedura, 
+    il blocco body e il nome del package
+    """     
+    package_name = None
+    
+    #Config and params
+    isInSignature = None
+    signatureLineIndex = None
+    
+    isInBody = None
+    bodyLineIndex = None
+    
+    isComment = None
+    isInCommentBlock = None
+    commentLineIndex = None
+    
+    #=======================================================================
+    # Strings which identify a procedure part. They will be requested from the gets to build a procedure record
+    #=======================================================================
+    procComment = str()
+    procSignature = str()
+    procBody = str()
+    
+    
+    line = str()
+    procs = list()        
+    procIndex = None
+    procedureParsed = tuple()
+    
+    for count, line in enumerate(f):
+        isComment, isInCommentBlock = _check_comment(line, isInCommentBlock) #value of the previous step
+        if isComment or isInCommentBlock:
+            #We are in a comment or in a comment block
+            procComment = _build_comment_block(line, count)
+            isInBody, isInSignature = False, False
+        else:
+            #We aren't in a comment
+            if not isInBody:
+                #Check and Eventually Process Signature
+                if not(isInSignature) and _check_is_in_signature(line, isInSignature): #value of the previous step
+                    #We are just entering into procedure signature
+                    procIndex = count
+                    isInSignature = True
+                    procSignature, signatureLineIndex = _build_signature_block(line, signatureLineIndex, procSignature, count)
+                elif isInSignature and _check_is_in_signature(line, isInSignature): #value of the previous step
+                    #We are still into procedure signature
+                    procSignature, signatureLineIndex = _build_signature_block(line, signatureLineIndex, procSignature, count)
+                
+                #This check must be done always because a signature can begin and terminate on the same line
+                if isInSignature and not _check_is_in_signature(line, isInSignature): #value of the previous step
+                    #We are just finished to read the signature
+                    isInSignature = False
+                    #build for the last time the signature block if necessary
+                    procSignature, signatureLineIndex = _build_signature_block(line, signatureLineIndex, procSignature, count)
+                    if not process_body:
+                        procedureParsed = (procIndex, procComment, procSignature, None)
+                        procComment = str()
+                        procSignature = str()
+                        procs.append(procedureParsed)
+                                        
+            if process_body and not isInSignature:
+                #Check and Eventually Process block
+                if not(isInBody) and _check_is_in_body_block(line, isInBody): #value of the previous step
+                    #We are just entering into procedure block
+                    isInBody = True
+                    procBody =  _build_procedure_body_block(line, count)
+                elif isInBody and _check_is_in_body_block(line, isInBody): #value of the previous step
+                    #We are still into procedure body
+                    procBody = _build_procedure_body_block(line, count)
+                elif isInBody and not _check_is_in_body_block(line, isInBody): #value of the previous step
+                    #We are just finished to read the body
+                    isInBody = False
+                    procedureParsed = (procIndex, procComment, procSignature, procBody)
+                    procComment = str()
+                    procSignature = str()
+                    procBody = str()
+                    procs.append(procedureParsed)
+    #end for                    
+    return procs
